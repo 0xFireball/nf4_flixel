2d3142fce03199ca258247d71f6e457d32e71ba1
0xFireball <0xFireball@outlook.com>
2017-07-26 20:26:02 -0700

    update pause button

--- src/nf4/ui/touch/NFPauseButton.hx
+++ src/nf4/ui/touch/NFPauseButton.hx
@@ -4,29 +4,28 @@ import flixel.*;
 
 class NFPauseButton extends FlxSprite {
 
-    public function new(?X:Float, ?Y:Float, Opacity:Float = 0.7) {
+    public function new(?X:Float, ?Y:Float, Opacity:Float = 0.7, Scale:Float = 1.0) {
         super(X, Y);
 
         loadGraphic(NF4AssetPaths.nf4_ui_pause__png);
         alpha = 0.7;
+        scale.set(Scale, Scale);
     }
 
     public function isPressed():Bool {
         var press:Bool = false;
 
-        var mousePos = FlxG.mouse.getPositionInCameraView(cameras[0]);
 
         #if !FLX_NO_MOUSE
-        press = press || mousePos.x > this.x && mousePos.x < this.x + this.width
-            && mousePos.y > this.y && mousePos.y < this.y + this.height;
+        var mousePos = FlxG.mouse.getPositionInCameraView(cameras[0]);
+        press = press || FlxG.mouse.justPressed && this.getBoundingBox(cameras[0]).containsPoint(mousePos);
         #end
 
 
         #if !FLX_NO_TOUCH
         var touch = FlxG.touches.getFirst();
-        if (touch != null) {
-            press = press || touch.x > this.x && touch.x < this.x + this.width
-            && touch.y > this.y && touch.y < this.y + this.height;
+        if (touch != null && touch.justPressed) {
+            press = press || this.getBoundingBox(cameras[0]).containsPoint(touch);
         }
         #end
 
